ccd576b12f3ccd42a4d9dc8c1cc23cb69a045688
Iuri Mendes <fcoiuri@gmail.com>
2022-08-24 21:36:26 -0300

    Adjusting deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import { Home } from "components/Home";
 import { ThemeProvider } from "@material-ui/core";
 import { theme } from "_assets/theme";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/Form-Recognicao" element={<Home />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
